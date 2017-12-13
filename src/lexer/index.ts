fd348718abde96bb830c10e2d39cef87db8bc5b9
Hyunje Jun <me@noraesae.net>
2017-12-13 17:38:59 +0900

    Change tokenize to handle and return iterables

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -1,6 +1,6 @@
 import { Token, Punctuation } from './token';
 
-export function tokenize(input: string): Array<Token<any>> {
+export function* tokenize(input: Iterable<string>): Iterable<Token<any>> {
   // FIXME
   return [new Punctuation(0, 0, '->')];
 }
