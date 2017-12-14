58c872e0f08ccd9fe730250feee2576626431827
Hyunje Jun <me@noraesae.net>
2017-12-14 18:10:16 +0900

    Use previewable in lexer

--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -1,6 +1,14 @@
+import { previewable } from 'previewable-iterator';
 import { Token, Punctuation } from './token';
 
 export function* tokenize(input: Iterable<string>): Iterable<Token<any>> {
-  // FIXME
+  // make input previewable
+  const charIter = previewable(input);
+
+  for (const char of charIter) {
+    console.log('current: ', char);
+    console.log('next: ', charIter.preview().value);
+  }
+
   return [new Punctuation(0, 0, '->')];
 }
