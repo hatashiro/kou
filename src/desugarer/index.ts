1a007728648a3d068d28f1d5e1713b1d8e4c8a71
Hyunje Jun <me@noraesae.net>
2018-01-18 18:22:26 +0900

    Use visitor in desugarer

--- src/desugarer/index.ts
+++ src/desugarer/index.ts
@@ -1,6 +1,9 @@
 import * as a from '../parser/ast';
+import { visitor } from '../parser/visitor';
 
 export function desugar(mod: a.Module): a.Module {
   // FIXME
-  return mod;
+  const v = visitor({});
+
+  return v.visitModule(mod);
 }
