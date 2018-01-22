f79c144263b4d1099c19db70c0947b4f5e2ce497
Hyunje Jun <me@noraesae.net>
2018-01-22 18:52:47 +0900

    Implement typeEqual for simple types

--- src/typechecker/index.ts
+++ src/typechecker/index.ts
@@ -15,11 +15,15 @@ export class TypeError extends Error {
   constructor(
     public row: number,
     public column: number,
-    public unexpected: string,
     public expected: string,
+    public actual: string,
+    message?: string,
   ) {
-    super();
-    this.message = '';
+    super(
+      `${message || 'Type mismatch'}: expected ${expected}, found ${
+        actual
+      } at ${row}:${column}`,
+    );
   }
 }
 
@@ -27,4 +31,38 @@ export function typeOf(expr: a.Expr<any>, ctx: TypeContext): a.Type<any> {
   return new a.VoidType(-1, -1);
 }
 
-export function typeEqual(expected: a.Type<any>, actual: a.Type<any>) {}
+export function typeEqual(expected: a.Type<any>, actual: a.Type<any>) {
+  // simple types
+  if (
+    (expected instanceof a.IntType && actual instanceof a.IntType) ||
+    (expected instanceof a.FloatType && actual instanceof a.FloatType) ||
+    (expected instanceof a.StrType && actual instanceof a.StrType) ||
+    (expected instanceof a.BoolType && actual instanceof a.BoolType) ||
+    (expected instanceof a.CharType && actual instanceof a.CharType) ||
+    (expected instanceof a.VoidType && actual instanceof a.VoidType)
+  ) {
+    return;
+  }
+
+  // func type
+  if (expected instanceof a.FuncType && actual instanceof a.FuncType) {
+    // FIXME
+  }
+
+  // tuple type
+  if (expected instanceof a.TupleType && actual instanceof a.TupleType) {
+    // FIXME
+  }
+
+  // list type
+  if (expected instanceof a.ListType && actual instanceof a.ListType) {
+    // FIXME
+  }
+
+  throw new TypeError(
+    actual.row,
+    actual.column,
+    actual.constructor.name,
+    expected.constructor.name,
+  );
+}
