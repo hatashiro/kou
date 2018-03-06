983ed2f74771ff8cc224dc96fdcd46861fa36404
Hyunje Jun <me@noraesae.net>
2018-03-06 18:23:15 +0900

    Implement kouc, a kou Programming Language Compiler

--- src/kouc.ts
+++ src/kouc.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
-import { readFileSync } from 'fs';
+import * as yargs from 'yargs';
+import { readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 
 import { tokenize } from './lexer';
@@ -11,12 +12,7 @@ import { typeCheck } from './typechecker';
 import { TypeContext } from './typechecker/context';
 import { TypeError } from './typechecker/error';
 import { Compose } from './util';
-
-const compile = Compose.then(tokenize)
-  .then(parse)
-  .then(desugarBefore)
-  .then(mod => typeCheck(mod, new TypeContext()))
-  .then(desugarAfter).f;
+import { genWAT, genWASM } from './codegen';
 
 function exitWithErrors(errors: Array<string>) {
   errors.forEach(err => console.error(err));
@@ -69,32 +65,74 @@ function reportCompileError(
   exitWithErrors(errors);
 }
 
-const relPaths = process.argv.slice(2);
-
-if (!relPaths.length) {
-  exitWithErrors(['No input file']);
+interface Argv {
+  wat: boolean;
+  out: string | null;
+  export: string;
+  source: string;
 }
 
-relPaths.forEach(path => {
-  const absPath = resolve(path);
+function main(argv: Argv) {
+  const exports = argv.export.split(',').map(e => e.trim());
+  const compile = Compose.then(tokenize)
+    .then(parse)
+    .then(desugarBefore)
+    .then(mod => typeCheck(mod, new TypeContext()))
+    .then(mod => (argv.wat ? genWAT(mod, exports) : genWASM(mod, exports))).f;
+
+  const sourcePath = resolve(argv.source);
 
   let input: string;
-
   try {
-    input = readFileSync(absPath, 'utf-8');
+    input = readFileSync(sourcePath, 'utf-8');
   } catch (err) {
     return exitWithErrors([
-      `Cannot open input file: ${path}`,
+      `Cannot open input file: ${sourcePath}`,
       '',
       chalk.red(err.message),
     ]);
   }
 
+  let output: string | Buffer;
   try {
-    compile(input);
+    output = compile(input);
   } catch (err) {
     return reportCompileError(input, err);
   }
-});
 
-console.log(chalk.green('Build succeeded!'));
+  // prettify a little
+  if (typeof output === 'string') {
+    output = output.replace(/ \)/g, ')').replace(/\( /g, '(');
+  }
+
+  if (argv.out) {
+    writeFileSync(argv.out, output);
+    console.log(chalk.green('Build succeeded!'));
+  } else {
+    console.log();
+    console.log(
+      Buffer.isBuffer(output) ? require('hexy').hexy(output) : output,
+    );
+  }
+}
+
+main(yargs
+  .usage('$0 [args] <source>', 'kou Programming Language Compiler')
+  .help()
+  .option('wat', {
+    desc: 'Output wat insteadof wasm',
+    type: 'boolean',
+    default: false,
+  })
+  .option('out', {
+    alias: 'o',
+    desc: 'Output file name, stdout if null',
+    type: 'string',
+    default: null,
+  })
+  .option('export', {
+    alias: 'e',
+    desc: 'Comma-separated list of export names',
+    type: 'string',
+    default: 'main',
+  }).argv as any);
