40f94de08c0a4776eea007c1b9dc5df3c81fc1b1
Hyunje Jun <me@noraesae.net>
2019-05-07 21:58:11 +0900

    Prevent codegen test to run tests in parallel
    There is a memory issue in Node.js 12.

--- test/codegen.spec.ts
+++ test/codegen.spec.ts
@@ -39,63 +39,64 @@ async function moduleRunTest(
   }
 }
 
-moduleRunTest(
-  `
+(async function() {
+  await moduleRunTest(
+    `
 let test = fn () float {
   1234.
 }
 `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () void {
   1234.;
 }
 `,
-  undefined,
-);
+    undefined,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234
 }
 `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   true
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () char {
   '\\n'
 }
 `,
-  '\n'.codePointAt(0),
-);
+    '\n'.codePointAt(0),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   let x = 1234;
   x
 }
 `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   let x = 1234;
   x;
@@ -103,11 +104,11 @@ let test = fn () int {
   y
 }
 `,
-  123,
-);
+    123,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let x = 1234
 let y = 123
 let test = fn () int {
@@ -115,11 +116,11 @@ let test = fn () int {
   y
 }
 `,
-  123,
-);
+    123,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test_impl = fn () int {
   let x = 1234;
   x;
@@ -129,11 +130,11 @@ let test_impl = fn () int {
 
 let test = test_impl
 `,
-  123,
-);
+    123,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test_impl = fn (x float) float {
   1234;
   x
@@ -143,11 +144,11 @@ let test = fn () float {
   test_impl(123.)
 }
 `,
-  123,
-);
+    123,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test_impl = fn (x float, y int) int {
   x;
   y
@@ -157,11 +158,11 @@ let test = fn () int {
   test_impl(123., test_impl(123., 1234))
 }
 `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test_impl = fn (x float, y int) int {
   x;
   y
@@ -173,11 +174,11 @@ let test = fn () int {
   g(123., f(123., 1234))
 }
 `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test_impl = fn (x float, y int) int {
   x;
   y
@@ -189,56 +190,56 @@ let test = fn () int {
   x
 }
 `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   -100
 }
 `,
-  -100,
-);
+    -100,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   +100.123
 }
 `,
-  100.123,
-);
+    100.123,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   -100.123
 }
 `,
-  -100.123,
-);
+    -100.123,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   !false
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   !true
 }
 `,
-  0,
-);
+    0,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let a = 1234.
 let f = fn () float {
   a;
@@ -262,218 +263,218 @@ let test = fn () float {
   c(true)
 }
 `,
-  1.23,
-);
+    1.23,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   1234 == 1234
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   1234. == 1234.
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   'a' != 'a'
 }
 `,
-  0,
-);
+    0,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   'a' < 'b'
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   'a' <= 'a'
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   1.234 > 12.34
 }
 `,
-  0,
-);
+    0,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   1234 >= 1235
 }
 `,
-  0,
-);
+    0,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234 + 5678
 }
 `,
-  6912,
-);
+    6912,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   1234. + .5678
 }
 `,
-  1234.5678,
-);
+    1234.5678,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234 - 5678 + 1
 }
 `,
-  -4443,
-);
+    -4443,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   1234. - 5678. + 1.
 }
 `,
-  -4443,
-);
+    -4443,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234 ^ 5678
 }
 `,
-  4860,
-);
+    4860,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234 | 5678
 }
 `,
-  5886,
-);
+    5886,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234 & 5678
 }
 `,
-  1026,
-);
+    1026,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234 * 5678
 }
 `,
-  7006652,
-);
+    7006652,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   1234. * 5678.
 }
 `,
-  7006652,
-);
+    7006652,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   -5678 / 1234
 }
 `,
-  -4,
-);
+    -4,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   4. / -2.
 }
 `,
-  -2,
-);
+    -2,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   1234 % 123
 }
 `,
-  4,
-);
+    4,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   -1234 % -123
 }
 `,
-  -4,
-);
+    -4,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   false && true
 }
 `,
-  0,
-);
+    0,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   true && (1 == 1)
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () bool {
   true || false
 }
 `,
-  1,
-);
+    1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let fac = fn (n int) int {
   if n == 1 {
     1
@@ -486,50 +487,53 @@ let test = fn () int {
   fac(10)
 }
 `,
-  3628800,
-);
+    3628800,
+  );
 
-const s2i = (slice: ArrayBuffer) => new Int32Array(slice)[0];
-const s2f = (slice: ArrayBuffer) => new Float64Array(slice)[0];
+  const s2i = (slice: ArrayBuffer) => new Int32Array(slice)[0];
+  const s2f = (slice: ArrayBuffer) => new Float64Array(slice)[0];
 
-const tuple = (...sizes: Array<number>) => ({ value, memory }: WASMResult) => {
-  let offset = value;
-  return sizes.map(size => {
-    const slice = memory.buffer.slice(offset, offset + size);
-    let value;
-    if (size === 8) {
-      // must be float
-      value = s2f(slice);
-    } else {
-      // must be 4, read as int32
-      value = s2i(slice);
-    }
-    offset += size;
-    return value;
-  });
-};
+  const tuple = (...sizes: Array<number>) => ({
+    value,
+    memory,
+  }: WASMResult) => {
+    let offset = value;
+    return sizes.map(size => {
+      const slice = memory.buffer.slice(offset, offset + size);
+      let value;
+      if (size === 8) {
+        // must be float
+        value = s2f(slice);
+      } else {
+        // must be 4, read as int32
+        value = s2i(slice);
+      }
+      offset += size;
+      return value;
+    });
+  };
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () (int, float, bool) {
   (1, 1.5, true)
 }
   `,
-  [1, 1.5, 1],
-  tuple(4, 8, 4),
-);
+    [1, 1.5, 1],
+    tuple(4, 8, 4),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () () {
   ()
 }
   `,
-  0,
-);
+    0,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let f1 = fn () () {
   ()
 }
@@ -545,72 +549,72 @@ let test = fn () ((), bool) {
   (f1(), f2(false))
 }
   `,
-  [0, 1],
-  tuple(4, 4),
-);
+    [0, 1],
+    tuple(4, 4),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   (1, 1.5, true)[1]
 }
   `,
-  1.5,
-);
+    1.5,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   int((1, 3.5, true)[1])
 }
   `,
-  3,
-);
+    3,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   let x = (1, (2, (3, 4), 5), 6, (7, 8));
   x[1][1][1]
 }
   `,
-  4,
-);
+    4,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () (float, float) {
   let x = (1, 1.5, (2.5, false, (), 0), 3.5);
   let y = (2.5, false);
   (x[2][0] - float(x[0]), y[0] + float(x[2][3]))
 }
   `,
-  [1.5, 2.5],
-  tuple(8, 8),
-);
+    [1.5, 2.5],
+    tuple(8, 8),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   let x = 1234.;
   int(x)
 }
   `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   let x = 1234;
   float(x)
 }
   `,
-  1234,
-);
+    1234,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   let x = 1;
   x = x + 10;
@@ -619,11 +623,11 @@ let test = fn () int {
   x
 }
   `,
-  15,
-);
+    15,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let g = 10
 
 let test = fn () int {
@@ -635,11 +639,11 @@ let test = fn () int {
   g
 }
   `,
-  25,
-);
+    25,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let g = (10, 20., 30)
 
 let test = fn () int {
@@ -648,100 +652,100 @@ let test = fn () int {
   int(g[1]) + g[2]
 }
   `,
-  60,
-);
+    60,
+  );
 
-const array = (size: number) => ({ value, memory }: WASMResult) => {
-  let offset = value;
+  const array = (size: number) => ({ value, memory }: WASMResult) => {
+    let offset = value;
 
-  const len = s2i(memory.buffer.slice(offset, offset + 4));
-  if (size === 8) {
-    // must be float
-    return Array.from(
-      new Float64Array(memory.buffer.slice(offset + 4, offset + 4 + 8 * len)),
-    );
-  } else {
-    // must be 4, read as int32
-    return Array.from(
-      new Int32Array(memory.buffer.slice(offset + 4, offset + 4 + 4 * len)),
-    );
-  }
-};
+    const len = s2i(memory.buffer.slice(offset, offset + 4));
+    if (size === 8) {
+      // must be float
+      return Array.from(
+        new Float64Array(memory.buffer.slice(offset + 4, offset + 4 + 8 * len)),
+      );
+    } else {
+      // must be 4, read as int32
+      return Array.from(
+        new Int32Array(memory.buffer.slice(offset + 4, offset + 4 + 4 * len)),
+      );
+    }
+  };
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () [int] {
   [1, 2, 3]
 }
   `,
-  [1, 2, 3],
-  array(4),
-);
+    [1, 2, 3],
+    array(4),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () [bool] {
   [true, false, true, true]
 }
   `,
-  [1, 0, 1, 1],
-  array(4),
-);
+    [1, 0, 1, 1],
+    array(4),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () [float] {
   [1.5, 2.4, 3.3, 4.2, 5.1]
 }
   `,
-  [1.5, 2.4, 3.3, 4.2, 5.1],
-  array(8),
-);
+    [1.5, 2.4, 3.3, 4.2, 5.1],
+    array(8),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   [1.5, 2.4, 3.3, 4.2, 5.1][2]
 }
   `,
-  3.3,
-);
+    3.3,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   [[1.5, 2.4], [3.3, 4.2, 5.1]][1][2]
 }
   `,
-  5.1,
-);
+    5.1,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () float {
   let x = [1.5, 2.4, 3.3, 4.2, 5.1];
   x[2] = x[1] + x[3];
   x[2]
 }
   `,
-  6.6,
-);
+    6.6,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () [float] {
   let x = [[1.5, 2.4], [3.3, 4.2, 5.1]];
   x[1] = x[0];
   x[1]
 }
   `,
-  [1.5, 2.4],
-  array(8),
-);
+    [1.5, 2.4],
+    array(8),
+  );
 
-// new expr
-moduleRunTest(
-  `
+  // new expr
+  await moduleRunTest(
+    `
 let test = fn () [float] {
   let y = (1, 2, 3);
   let x = new float[y[2]];
@@ -751,34 +755,34 @@ let test = fn () [float] {
   x
 }
   `,
-  [1.5, 2.5, 3.5],
-  array(8),
-);
+    [1.5, 2.5, 3.5],
+    array(8),
+  );
 
-// len
-moduleRunTest(
-  `
+  // len
+  await moduleRunTest(
+    `
 let test = fn () int {
   len([true, false, true, false, false])
 }
   `,
-  5,
-);
+    5,
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   let y = (1, 2, 3);
   let x = new float[y[2]];
   len(x)
 }
   `,
-  3,
-);
+    3,
+  );
 
-// loop
-moduleRunTest(
-  `
+  // loop
+  await moduleRunTest(
+    `
 let test = fn () int {
   let x = 0;
   while x < 100 {
@@ -787,10 +791,10 @@ let test = fn () int {
   x
 }
   `,
-  100,
-);
-moduleRunTest(
-  `
+    100,
+  );
+  await moduleRunTest(
+    `
 let test = fn () int {
   let x = 0;
   while x < 100 {
@@ -803,10 +807,10 @@ let test = fn () int {
   x
 }
   `,
-  80,
-);
-moduleRunTest(
-  `
+    80,
+  );
+  await moduleRunTest(
+    `
 let test = fn () [float] {
   let y = (1, 2, 3);
   let x = new float[y[2]];
@@ -818,11 +822,11 @@ let test = fn () [float] {
   x
 }
   `,
-  [0.5, 1.5, 2.5],
-  array(8),
-);
-moduleRunTest(
-  `
+    [0.5, 1.5, 2.5],
+    array(8),
+  );
+  await moduleRunTest(
+    `
 let test = fn () [int] {
   let res = new int[5];
   let i = 1;
@@ -839,12 +843,12 @@ let test = fn () [int] {
   res
 }
   `,
-  [1, 3, 6, 10, 15],
-  array(4),
-);
+    [1, 3, 6, 10, 15],
+    array(4),
+  );
 
-moduleRunTest(
-  `
+  await moduleRunTest(
+    `
 let test = fn () int {
   other()
 }
@@ -853,7 +857,8 @@ let other = fn () int {
   1234
 }
   `,
-  1234,
-);
+    1234,
+  );
+})();
 
 console.log(chalk.green.bold('Passed!'));
